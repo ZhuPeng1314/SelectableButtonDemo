c9d99bc33eb7d11f8cb8ed7a795ce332e1635284
ZhuPeng1314 <mo.shang1@qq.com>
2015-12-22 11:28:45 +0800

    具有Selected状态的按钮
    从Normal进入Selected，会自动交换button的前景色和背景色
    所以需要把按钮的默认前景色设为透明

---
+++ ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SelectableButtonDemo
+//
+//  Created by 鹏 朱 on 15/12/22.
+//  Copyright © 2015年 鹏 朱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 从Normal进入Selected，会自动交换button的前景色和背景色
+        // 所以在此处把前景色设为透明
+        button1.tintColor = UIColor.clearColor()
+        
+        button1.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+        button1.setTitleColor(UIColor.redColor(), forState: UIControlState.Selected)
+        
+        // 需要在StoryBoard当中固定按钮的高宽约束，以免图片View对button的高宽产生不必要的影响。
+        button1.setBackgroundImage(UIImage(named: "button-selected"), forState: UIControlState.Selected)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+    @IBAction func button1Pressed(sender: UIButton) {
+        sender.selected = !sender.selected
+    }
+}
+
