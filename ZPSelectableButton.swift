9a926011a9ada20de6c1b03b662411429dc61942
ZhuPeng1314 <mo.shang1@qq.com>
2015-12-22 13:56:47 +0800

    将真实的选中状态属性与UIControl中的selected关联起来
    selectedTruely暴露给外部，当调用者直接修改该属性时会同时修改UIControl中的selected

--- ZPSelectableButton.swift
+++ ZPSelectableButton.swift
@@ -11,7 +11,16 @@ import UIKit
 class ZPSelectableButton: UIButton {
     
     //真实的选中状态
-    var selectedTruely = false
+    private var selectedTruely1 = false
+    var selectedTruely:Bool { //selectedTruely暴露给外部，当调用者直接修改该属性时会同时修改UIControl中的selected
+        get {
+            return selectedTruely1
+        }
+        set (newSelected) {
+            self.selectedTruely1 = newSelected
+            self.selected = newSelected //同时改变UIControl中的selected状态为目标状态
+        }
+    }
     // UIControl中的selected只是为了表示当前UI是否应该处于selected UI
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -28,7 +37,6 @@ class ZPSelectableButton: UIButton {
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.selectedTruely = !self.selectedTruely //改变真实的选中状态
-        self.selected = self.selectedTruely //改变selected状态为目标状态
         super.touchesEnded(touches, withEvent: event)
     }
 
